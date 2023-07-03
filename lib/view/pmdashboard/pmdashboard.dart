f5b4ac63cb7e0a9f47f8c9063b454b3c15a76fc7
Raifatk <19ncs11@meaec.edu.in>
2023-07-03 14:17:45 +0530

    first commit

---
+++ lib/view/pmdashboard/pmdashboard.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+class pmdashboard extends StatelessWidget {
+  const pmdashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const TodayText(),
+        const SizedBox(width: kSpacing),
+        Expanded(child: SearchField()),
+      ],
+    );
+  }
+}
